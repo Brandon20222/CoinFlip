4e8445b65006e46846bb57c9531391929c255807
Brandon20222 <bbiltgen@cub.uca.edu>
2025-07-14 11:08:34 -0500

    finished Coin class cpp

--- Coin.cpp
+++ Coin.cpp
@@ -1,8 +1,27 @@
 #include "Coin.h"
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
 Coin::Coin() : centValue(1), isHeads(false){
 }
-Coin::Coin(int value) {
 
+Coin::Coin(int value) : centValue(value),isHeads(false) {
+}
+
+void Coin::flip() {
+	isHeads = rand() % 2 == 0;
+}
+bool Coin::isItHeads()const {
+	return isHeads;
+}
+string Coin::getSideUp()const {
+	if (isHeads)
+		return "Heads";
+	else
+		return "Tails";
+}
+int Coin::getCentValue()const {
+	return centValue;
 }
