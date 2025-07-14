e321c00a49c5bdd5b2d8fe7604286acc91fb78b9
Brandon20222 <bbiltgen@cub.uca.edu>
2025-07-14 10:35:51 -0500

    Coin header file

--- Coin.h
+++ Coin.h
@@ -1,8 +1,20 @@
 #ifndef COIN_H
 #define COIN_H
+#include <string>
+#include <iostream>
 class Coin
 {
+private:
+	bool isHeads;
+	int const cenValue;
 public:
+	Coin();
+	Coin(int value);
+	void flip();
+	bool isItHeads()const;
+	string getSideUp()const;
+	int getCentValue()const;
+	
 };
 
 #endif
